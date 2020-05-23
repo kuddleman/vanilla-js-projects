c945464810260ee9312ccef58c6507751564a6a3
Kuddleman <edl1155@gmail.com>
2020-05-23 13:18:18 -0700

    complete day 3

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -1,1 +1,24 @@
 console.log('hello')
+
+//get all passwords, current and new
+//turn the node list into an array so I can apply forEach to the array:
+let passwords = Array.prototype.slice.call(document.querySelectorAll('[ type="password" ]'))
+
+let toggle = document.querySelector('#show-passwords')
+
+//assign click event to toggle;
+
+toggle.addEventListener('click', ( event ) => {
+   // loop through passwords:
+   // if box is checked, show text.  If not, hide password
+   passwords.forEach( password => {
+    if ( toggle.checked ) {
+      password.type = 'text'
+    } else {
+      password.type = 'password'
+    }
+   })
+     
+   
+})
+
